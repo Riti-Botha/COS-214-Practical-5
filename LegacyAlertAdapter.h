4a392eea0f89770503cf08fa4e81c2d1cb0016b6
RecursiveAsh <boitu.ashley13@gmail.com>
2026-09-15 22:48:01 +0200

    Added Adapter header file

---
+++ LegacyAlertAdapter.h
@@ -0,0 +1,21 @@
+#ifndef LEGACYALERTADAPTER_H
+#define LEGACYALERTADAPTER_H
+#include "AlertService.h"
+#include <string>
+
+class LegacyAlertSystem;
+class LegacyAlertAdapter : public AlertService {
+    private:
+        LegacyAlertSystem* legacy;                       // owned
+
+        int zoneFor(const std::string& location) const;
+        int severityFor(const std::string& message) const;
+
+    public:
+        LegacyAlertAdapter(LegacyAlertSystem* legacySystem);
+        virtual ~LegacyAlertAdapter();
+
+        virtual bool sendAlert(const std::string& location, const std::string& message);
+};
+
+#endif
