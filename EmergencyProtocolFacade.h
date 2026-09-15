911818a3d1de35f0a07e9e4bc252fda79a11cf45
RecursiveAsh <boitu.ashley13@gmail.com>
2026-09-15 22:51:31 +0200

    Added Facade header file

---
+++ EmergencyProtocolFacade.h
@@ -0,0 +1,23 @@
+#ifndef EMERGENCYPROTOCOLFACADE_H
+#define EMERGENCYPROTOCOLFACADE_H
+
+class ResponseComponent;
+class ResponseUnitFactory;
+class AlertService;
+class OperatorCommand;
+
+class EmergencyProtocolFacade {
+    private:
+        ResponseComponent* security;
+        ResponseComponent* medical;
+        ResponseUnitFactory* unitFactory;
+        AlertService* alertService;
+
+    public:
+        EmergencyProtocolFacade(ResponseComponent* security, ResponseComponent* medical, ResponseUnitFactory* unitFactory, AlertService* alertService);
+        ~EmergencyProtocolFacade();
+        bool activateEmergencyProtocol(const std::string& location, OperatorCommand* lockdownCommand);
+        bool evacuate(const std::string& location);
+};
+
+#endif
